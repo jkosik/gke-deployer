be3e45fd37e3e39e438c8116215740a94ccdb169
jkosik <juraj.kosik@gmail.com>
2021-07-02 18:15:38 +0200

    v2

--- infrastructure/import-kubeconfig-secret.sh
+++ infrastructure/import-kubeconfig-secret.sh
@@ -2,7 +2,7 @@
 
 # Importer of the Secret to the Terraform
 # Jumphost downloads GKE kubeconfig and stores it to the GCP Secret Manager. Jumphost needs kubeconfig anyhow.
-# We could manage kubeconfig within TF. TF does not provide resource for getting GKE kubeconfig TF local-exec is cumbersome.
+# We could manage kubeconfig within TF. TF does not provide resource for getting GKE kubeconfig and TF local-exec is cumbersome.
 
 if [ $# -eq 0 ]; then
     echo "Environment not provided."
@@ -10,9 +10,9 @@ if [ $# -eq 0 ]; then
 fi
 
 INFRA_ENV=$1
-PROJECT_ID=$(cat dev.tfvars | grep project_id | cut -d\" -f2)
+PROJECT_ID=$(cat $INFRA_ENV.tfvars | grep project_id | cut -d\" -f2)
 PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PROJECT_NUMBER)")
-GKE_CLUSTER_NAME=$(cat dev.tfvars | grep gke_cluster_name | cut -d\" -f2)
+GKE_CLUSTER_NAME=$(cat $INFRA_ENV.tfvars | grep gke_cluster_name | cut -d\" -f2)
 
 terraform import -var-file=$INFRA_ENV.tfvars -allow-missing-config google_secret_manager_secret.kubeconfig projects/$PROJECT_NUMBER/secrets/kubeconfig-$GKE_CLUSTER_NAME
 
